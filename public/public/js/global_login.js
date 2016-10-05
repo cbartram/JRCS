18e2f2d1cb337f6a5f0326a60f4e339ff7e7a30a
Christian <alaproductions.staff@gmail.com>
2016-10-05 15:13:16 -0400

    Added vendor to git format timestamps for CICO

--- public/public/js/global_login.js
+++ public/public/js/global_login.js
@@ -62,6 +62,7 @@ $(document).ready(function() {
         //todo needs AM/PM and its in 24 hour time needs converting to 12 hour time
         var date = new Date();
         timestamp = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+        timestamp = formatDate(timestamp);
         email = $("#volunteer-email").val();
 
         if($("#volunteer-type").find(':selected').attr('name') == "default" || $("#volunteer-email").val().length == 0) {
@@ -115,3 +116,33 @@ $(document).ready(function() {
 
 
 });
+
+function formatDate(date) {
+    var d = new Date(date);
+    var hh = d.getHours();
+    var m = d.getMinutes();
+    var s = d.getSeconds();
+    var dd = "AM";
+    var h = hh;
+    if (h >= 12) {
+        h = hh-12;
+        dd = "PM";
+    }
+    if (h == 0) {
+        h = 12;
+    }
+    m = m<10?"0"+m:m;
+
+    s = s<10?"0"+s:s;
+
+    /* if you want 2 digit hours:
+     h = h<10?"0"+h:h; */
+
+    var pattern = new RegExp("0?"+hh+":"+m+":"+s);
+
+    var replacement = h+":"+m;
+    replacement += ":"+s;
+    replacement += " "+dd;
+
+    return date.replace(pattern,replacement);
+}
