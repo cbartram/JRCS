d11b5938861578dda3b5e277fa6d4c46271fec3c
Christian <alaproductions.staff@gmail.com>
2016-11-17 14:05:51 -0500

    Edited styles for add-volunteer modal

--- resources/views/components/modals/add-volunteer.blade.php
+++ resources/views/components/modals/add-volunteer.blade.php
@@ -8,43 +8,59 @@
             </div>
             <div class="modal-body">
                 {{ Form::open(array('url' => '/add', 'method' => 'post')) }}
-                <div class="input-group">
-                    <label for="first_name">Add a Volunteer</label>
-                    <p>{{ Form::text('first_name', Input::old('email'), array('placeholder' => 'First Name', 'class' => 'form-control')) }}</p>
-                    <p>{{ Form::text('last_name', Input::old('email'), array('placeholder' => 'Last Name', 'class' => 'form-control')) }}</p>
-                    <p>{{ Form::text('address', Input::old('email'), array('placeholder' => 'Address', 'class' => 'form-control')) }}</p>
-                    <p>{{ Form::text('city', Input::old('email'), array('placeholder' => 'City', 'class' => 'form-control')) }}</p>
-                    <p>{{ Form::text('state', Input::old('email'), array('placeholder' => 'State', 'class' => 'form-control')) }}</p>
-                    <p>{{ Form::text('zip', Input::old('email'), array('placeholder' => 'Zip Code', 'class' => 'form-control')) }}</p>
-                    <p>{{ Form::text('email', Input::old('email'), array('placeholder' => 'Email', 'class' => 'form-control')) }}</p>
-                    <p>{{ Form::text('phone', Input::old('email'), array('placeholder' => 'Phone', 'class' => 'form-control')) }}</p>
-                    <p>{{ Form::select('volunteer_type', array('General' => 'General', 'Program' => 'Program', 'Board' => 'Board'), 'General'), array('class' => 'form-control')}}</p>
 
-                    @if (Helpers::hasAccessTo('BEBCO', Session::get('id')))
-                        <div class="checkbox">
-                            <label>{{ Form::checkbox('bebco-checkbox', 'true') }} Add to BEBCO</label>
-                        </div>
-                    @endif
-                    @if (Helpers::hasAccessTo('JACO', Session::get('id')))
-                        <div class="checkbox">
-                            <label>{{ Form::checkbox('jaco-checkbox', 'true') }} Add to JACO</label>
 
-                        </div>
-                    @endif
-                    @if (Helpers::hasAccessTo('JBC', Session::get('id')))
-                        <div class="checkbox disabled">
-                            <label>{{ Form::checkbox('jbc-checkbox', 'true') }} Add to JBC</label>
-                        </div>
-                    @endif
+                <div class="row">
+                    <div class="col-lg-6">
+                        <label for="first_name">Add a Volunteer</label>
+                        <p>{{ Form::text('first_name', Input::old('first_name'), array('placeholder' => 'First Name', 'class' => 'form-control')) }}</p>
+                        <p>{{ Form::text('last_name', Input::old('last_name'), array('placeholder' => 'Last Name', 'class' => 'form-control')) }}</p>
+                        <p>{{ Form::text('address', Input::old('address'), array('placeholder' => 'Address', 'class' => 'form-control')) }}</p>
+                        <p>{{ Form::text('city', Input::old('city'), array('placeholder' => 'City', 'class' => 'form-control')) }}</p>
+                        <p>{{ Form::text('state', Input::old('state'), array('placeholder' => 'State', 'class' => 'form-control')) }}</p>
+                        <p>{{ Form::text('zip', Input::old('zip'), array('placeholder' => 'Zip Code', 'class' => 'form-control')) }}</p>
+                        <p>{{ Form::text('email', Input::old('email'), array('placeholder' => 'Email', 'class' => 'form-control')) }}</p>
+                        <p>{{ Form::text('phone', Input::old('phone'), array('placeholder' => 'Phone', 'class' => 'form-control')) }}</p>
+                        <p>
+                            <select name="volunteer_type" class="form-control">
+                                <option>General</option>
+                                <option>Program</option>
+                                <option>Board</option>
+                            </select>
+                        </p>
+                    </div>
+                    <div class="col-lg-6">
+                        <label for="checkbox">Volunteer Access</label>
+                        @if(Helpers::hasAccessTo('BEBCO', Session::get('id')))
+                            <div class="checkbox">
+                                <label>{{ Form::checkbox('bebco-checkbox', 'true') }} Add to BEBCO</label>
+                            </div>
+                        @endif
+                        @if(Helpers::hasAccessTo('JACO', Session::get('id')))
+                            <div class="checkbox">
+                                <label>{{ Form::checkbox('jaco-checkbox', 'true') }} Add to JACO</label>
 
-                </div><!-- /input-group -->
-                <div class="input-group">
-                    {{ Form::submit('Submit', array('class' => 'btn btn-primary')) }}
+                            </div>
+                        @endif
+                        @if(Helpers::hasAccessTo('JBC', Session::get('id')))
+                            <div class="checkbox disabled">
+                                <label>{{ Form::checkbox('jbc-checkbox', 'true') }} Add to JBC</label>
+                            </div>
+                        @endif
+                    </div>
                 </div>
                 {{Form::close()}}
             </div>
+
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="fa fa-times"></span> Close</button>
+                <div class="row">
+                    <div class="col-lg-3 col-lg-offset-7">
+                        <button type="submit" class="btn btn-primary" name="Submit"><i style="margin-top:4px; margin-left:5px;" class="fa fa-plus"></i> Add</button>
+                    </div>
+                    <div class="col-md-2">
+                        <button type="button" class="btn btn-default" data-dismiss="modal"><span class="fa fa-times"></span> Close</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
