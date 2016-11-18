6c982bd96cb71f9d5f3ff9dc03029fa4e14fab60
Christian <n00873419@ospreys.unf.edu>
2016-11-18 10:59:32 -0500

    Highchart improvements

--- resources/views/cico.blade.php
+++ resources/views/cico.blade.php
@@ -14,7 +14,7 @@
    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
     <!-- Custom styles for this template -->
-    <link href="../../public/css/Login.css" rel="stylesheet">
+    <link href="../../public/css/CICO.css" rel="stylesheet">
 </head>
 
 <body>
